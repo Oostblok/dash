f087d7d37c1ebb4726b15055a2be7312873ca988
fonzie <rudi@softmedia.nl>
2026-04-29 12:47:48 +0300

    Embed DHU

--- include/app/pages/dhu.hpp
+++ include/app/pages/dhu.hpp
@@ -1,16 +1,84 @@
 #pragma once
 
-#include <QWidget>
-#include <QPointer>
+#include <QtWidgets>
+#include <QStackedWidget>
+#include <QProcess>
+#include <QVBoxLayout>
+#include <QSvgWidget>
+#include <functional>
+#include <thread>
+#include <vector>
+
+#include <libusb-1.0/libusb.h>
+#include <boost/asio.hpp>
+
+#include "aasdk/USB/USBWrapper.hpp"
+#include "aasdk/USB/AccessoryModeQueryFactory.hpp"
+#include "aasdk/USB/AccessoryModeQueryChainFactory.hpp"
+#include "aasdk/USB/ConnectedAccessoriesEnumerator.hpp"
+#include "aasdk/USB/USBHub.hpp"
+#include "aasdk/TCP/TCPWrapper.hpp"
+#include "openauto/App.hpp"
+#include "openauto/Service/IAndroidAutoEntityFactory.hpp"
+#include "openauto/Service/IAndroidAutoEntity.hpp"
 
 #include "app/pages/page.hpp"
 
-class DHUPage : public Page {
+class Arbiter;
+
+class DummyAndroidAutoEntity : public openauto::service::IAndroidAutoEntity {
+public:
+    void start(openauto::service::IAndroidAutoEntityEventHandler&) override {}
+    void stop() override {}
+};
+
+class DummyAndroidAutoEntityFactory : public openauto::service::IAndroidAutoEntityFactory {
+public:
+    openauto::service::IAndroidAutoEntity::Pointer create(aasdk::usb::IAOAPDevice::Pointer) override {
+        return std::make_shared<DummyAndroidAutoEntity>();
+    }
+    openauto::service::IAndroidAutoEntity::Pointer create(aasdk::tcp::ITCPEndpoint::Pointer) override {
+        return std::make_shared<DummyAndroidAutoEntity>();
+    }
+};
+
+class AAWorker : public QObject {
+    Q_OBJECT
+
+public:
+    AAWorker(std::function<void(bool)> callback, QObject *parent = nullptr);
+    ~AAWorker();
+
+private:
+    void create_usb_workers();
+    void create_io_service_workers();
+    void waitForDevice();
+
+    std::function<void(bool)> callback;
+    libusb_context *usb_context = nullptr;
+    boost::asio::io_service io_service;
+    boost::asio::io_service::work work;
+    aasdk::tcp::TCPWrapper tcp_wrapper;
+    aasdk::usb::USBWrapper usb_wrapper;
+    aasdk::usb::AccessoryModeQueryFactory query_factory;
+    aasdk::usb::AccessoryModeQueryChainFactory query_chain_factory;
+    std::shared_ptr<aasdk::usb::USBHub> usb_hub;
+    std::shared_ptr<aasdk::usb::ConnectedAccessoriesEnumerator> connected_accessories_enumerator;
+    boost::asio::io_service::strand strand_;
+    std::vector<std::thread> thread_pool;
+};
+
+class DHUPage : public QStackedWidget, public Page {
+    Q_OBJECT
+
 public:
     DHUPage(Arbiter &arbiter);
     void init() override;
 
 private:
-    QWidget *root_ = nullptr;
-    QWidget *dhuContainer_ = nullptr;
+    void launchDHU(QWidget *root, QVBoxLayout *layout, QLabel *loader);
+    void killDHU();
+
+    AAWorker *worker = nullptr;
+    QProcess *dhuProcess = nullptr;
 };
--- src/app/pages/dhu.cpp
+++ src/app/pages/dhu.cpp
@@ -1,5 +1,6 @@
 #include "app/pages/dhu.hpp"
 #include <QVBoxLayout>
+#include <QLabel>
 #include <QFile>
 #include <QSvgWidget>
 #include <QProcess>
@@ -24,9 +25,8 @@ static unsigned long getWindowPID(Display *display, Window w)
     unsigned long nitems, bytesAfter;
     unsigned char *prop = nullptr;
 
-    if (XGetWindowProperty(display, w, atom, 0, 1, False, AnyPropertyType, &actualType, &format, &nitems, &bytesAfter, &prop) != Success) {
+    if (XGetWindowProperty(display, w, atom, 0, 1, False, AnyPropertyType, &actualType, &format, &nitems, &bytesAfter, &prop) != Success)
         return 0;
-    }
 
     unsigned long pid = 0;
     if (prop) {
@@ -77,96 +77,198 @@ static Window findDHUWindowRecursive(Display *display, unsigned long targetPid, 
     return result;
 }
 
-static Window findDHUWindow(Display *display, unsigned long targetPid) {
+static Window findDHUWindow(Display *display, unsigned long targetPid)
+{
     return findDHUWindowRecursive(display, targetPid, DefaultRootWindow(display));
 }
 
-DHUPage::DHUPage(Arbiter &arbiter)
-    : Page(arbiter, "DHU", "directions_car", false, new QWidget())
+AAWorker::AAWorker(std::function<void(bool)> callback, QObject *parent)
+    : QObject(parent)
+    , callback(callback)
+    , work(io_service)
+    , usb_wrapper((libusb_init(&usb_context), usb_context))
+    , query_factory(usb_wrapper, io_service)
+    , query_chain_factory(usb_wrapper, io_service, query_factory)
+    , usb_hub(std::make_shared<aasdk::usb::USBHub>(usb_wrapper, io_service, query_chain_factory))
+    , connected_accessories_enumerator(std::make_shared<aasdk::usb::ConnectedAccessoriesEnumerator>(usb_wrapper, io_service, query_chain_factory))
+    , strand_(io_service)
 {
-    QWidget *root = this->container()->take();
+    this->create_usb_workers();
+    this->create_io_service_workers();
+    this->waitForDevice();
+}
 
-    auto layout = new QVBoxLayout(root);
-    layout->setAlignment(Qt::AlignCenter);
+AAWorker::~AAWorker()
+{
+    this->usb_hub->cancel();
+    this->io_service.stop();
+    std::for_each(this->thread_pool.begin(), this->thread_pool.end(), std::bind(&std::thread::join, std::placeholders::_1));
+    libusb_exit(this->usb_context);
+}
+
+void AAWorker::create_usb_workers()
+{
+    std::function<void()> worker = [this]() {
+        timeval event_timeout = {180, 0};
+        while (!this->io_service.stopped())
+            libusb_handle_events_timeout_completed(this->usb_context, &event_timeout, nullptr);
+    };
+
+    this->thread_pool.emplace_back(worker);
+    this->thread_pool.emplace_back(worker);
+    this->thread_pool.emplace_back(worker);
+    this->thread_pool.emplace_back(worker);
+}
+
+void AAWorker::create_io_service_workers()
+{
+    std::function<void()> worker = [this]() { this->io_service.run(); };
+
+    this->thread_pool.emplace_back(worker);
+    this->thread_pool.emplace_back(worker);
+    this->thread_pool.emplace_back(worker);
+    this->thread_pool.emplace_back(worker);
+}
+
+void AAWorker::waitForDevice()
+{
+    auto promise = aasdk::usb::IUSBHub::Promise::defer(strand_);
+    promise->then(
+        [this](aasdk::usb::DeviceHandle) {
+            QMetaObject::invokeMethod(this, [this]() {
+                this->callback(true);
+            }, Qt::QueuedConnection);
+        },
+        [this](const aasdk::error::Error& error) {
+            if (error != aasdk::error::ErrorCode::OPERATION_ABORTED)
+                this->waitForDevice();
+        }
+    );
+    this->usb_hub->start(std::move(promise));
+}
+
+// ---- DHUPage ----
+
+DHUPage::DHUPage(Arbiter &arbiter)
+    : QStackedWidget()
+    , Page(arbiter, "AA", "directions_car", false, this)
+{
+}
+
+void DHUPage::init()
+{
+    QWidget *waiting = new QWidget(this);
+    QVBoxLayout *waitingLayout = new QVBoxLayout(waiting);
+    waitingLayout->setAlignment(Qt::AlignCenter);
 
     QFile file(":/graphics/dc.svg");
     file.open(QIODevice::ReadOnly);
     QString svgData = file.readAll();
-
-    QString color = "#ffffff"; // TODO: adjust to dark/light mode
+    QString color = "#ffffff"; // TODO: adjust to dark/light mode from Session
     svgData.replace("currentColor", color);
 
-    auto svg = new QSvgWidget();
-    svg->load(svgData.toUtf8());
+    auto logo = new QSvgWidget(waiting);
+    logo->load(svgData.toUtf8());
+    logo->setFixedSize(128, 88); // TODO: make size dynamic
+    waitingLayout->addWidget(logo, 0, Qt::AlignCenter);
 
-    layout->addWidget(svg, 0, Qt::AlignCenter);
+    waitingLayout->addSpacing(12);
 
-    this->container()->reset();
+    QLabel *connectLabel = new QLabel("Connect your phone to start Android Auto", waiting);
+    connectLabel->setAlignment(Qt::AlignCenter);
+    waitingLayout->addWidget(connectLabel, 0, Qt::AlignCenter);
 
-    QTimer::singleShot(3000, [=]() {
-        QString dhuPath = QDir::homePath()
-            + "/Android/Sdk/extras/google/auto/desktop-head-unit";
+    this->addWidget(waiting);
 
-        auto proc = new QProcess(root);
-        proc->start(dhuPath, {"-u"});
+    QWidget *dhuContainer = new QWidget(this);
+    QVBoxLayout *dhuLayout = new QVBoxLayout(dhuContainer);
+    dhuLayout->setAlignment(Qt::AlignCenter);
 
-        if (!proc->waitForStarted(3000)) {
-            qWarning() << "DHU failed to start";
+    // TODO: animate "blocks" in DC logo --> svg css animation by class?
+    QLabel *loader = new QLabel("Loading...", dhuContainer);
+    loader->setAlignment(Qt::AlignCenter);
+    dhuLayout->addWidget(loader, 0, Qt::AlignCenter);
+
+    this->addWidget(dhuContainer);
+    this->setCurrentIndex(0);
+
+    std::function<void(bool)> callback = [this, dhuContainer, dhuLayout, loader](bool connected) {
+        if (connected) {
+            this->setCurrentIndex(1);
+            this->launchDHU(dhuContainer, dhuLayout, loader);
+        } else {
+            this->killDHU();
+            this->setCurrentIndex(0);
+        }
+    };
+
+    this->worker = new AAWorker(callback, this);
+}
+
+void DHUPage::launchDHU(QWidget *root, QVBoxLayout *layout, QLabel *loader)
+{
+    QString dhuPath = QDir::homePath() + "/Android/Sdk/extras/google/auto/desktop-head-unit";
+
+    this->dhuProcess = new QProcess(root);
+    this->dhuProcess->start(dhuPath, {"-u"});
+
+    if (!this->dhuProcess->waitForStarted(3000)) {
+        qWarning() << "DHU failed to start";
+        return;
+    }
+
+    unsigned long pid = this->dhuProcess->processId();
+
+    QTimer *poll = new QTimer(root);
+    poll->setInterval(300);
+
+    QObject::connect(poll, &QTimer::timeout, [=]() mutable {
+        Display *display = XOpenDisplay(nullptr);
+        if (!display) return;
+
+        Window win = findDHUWindow(display, pid);
+        if (!win) {
+            XCloseDisplay(display);
             return;
         }
 
-        unsigned long pid = proc->processId();
+        poll->stop();
+        poll->deleteLater();
 
-        QTimer *poll = new QTimer(root);
-        poll->setInterval(300);
+        Atom hints = XInternAtom(display, "_MOTIF_WM_HINTS", False);
+        struct {
+            unsigned long flags, functions, decorations;
+            long input_mode;
+            unsigned long status;
+        } mwmHints = {2, 0, 0, 0, 0};
+        XChangeProperty(display, win, hints, hints, 32, PropModeReplace, (unsigned char *)&mwmHints, 5);
 
-        QObject::connect(poll, &QTimer::timeout, [=]() mutable {
-            Display *display = XOpenDisplay(nullptr);
-            if (!display) {
-                qWarning() << "display not available";
-                return;
-            }
+        XUnmapWindow(display, win);
+        XFlush(display);
 
-            Window win = findDHUWindow(display, pid);
-            if (!win) {
-                XCloseDisplay(display);
-                return;
-            }
+        XReparentWindow(display, win, (Window)root->winId(), 0, 0);
+        XResizeWindow(display, win, DHU_W, DHU_H);
+        XMapWindow(display, win);
+        XFlush(display);
+        XCloseDisplay(display);
 
-            poll->stop();
-            poll->deleteLater();
+        QWindow *external = QWindow::fromWinId(win);
+        QWidget *container = QWidget::createWindowContainer(external, root);
+        container->setFixedSize(DHU_W, DHU_H);
+        container->setFocusPolicy(Qt::NoFocus);
+        layout->addWidget(container, 0, Qt::AlignCenter);
 
-            Atom hints = XInternAtom(display, "_MOTIF_WM_HINTS", False);
-            struct {
-                unsigned long flags, functions, decorations;
-                long input_mode;
-                unsigned long status;
-            } mwmHints = {2, 0, 0, 0, 0};
-            XChangeProperty(display, win, hints, hints, 32, PropModeReplace, (unsigned char *)&mwmHints, 5);
-
-            XUnmapWindow(display, win);
-            XFlush(display);
-
-            XReparentWindow(display, win, (Window)root->winId(), 0, 0);
-            XResizeWindow(display, win, DHU_W, DHU_H);
-            XMapWindow(display, win);
-            XFlush(display);
-            XCloseDisplay(display);
-
-            QWindow *external = QWindow::fromWinId(win);
-            QWidget *container = QWidget::createWindowContainer(external, root);
-            container->setFixedSize(DHU_W, DHU_H);
-            container->setFocusPolicy(Qt::NoFocus);
-            layout->addWidget(container, 0, Qt::AlignCenter);
-
-            svg->deleteLater();
-        });
-
-        poll->start();
+        loader->deleteLater();
     });
+
+    poll->start();
 }
 
-void DHUPage::init() {
-  qDebug() << "Session::Theme::Dark";
-//   qDebug() << Session::Theme::Dark;
+void DHUPage::killDHU()
+{
+    if (this->dhuProcess) {
+        this->dhuProcess->terminate();
+        this->dhuProcess->waitForFinished(2000);
+        this->dhuProcess = nullptr;
+    }
 }
